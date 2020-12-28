354e81df0622b28eaae87441f60002cae74ee5dd
vietnam1995 <76698627+vietnam1995@users.noreply.github.com>
2020-12-28 23:51:10 +0700

    Update FacebookAutoLikeProfessional.meta.js

--- FacebookAutoLikeProfessional.meta.js
+++ FacebookAutoLikeProfessional.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://zrftech.blogspot.com
-// @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeProfessional.user.js
+// @downloadURL 	https://raw.githubusercontent.com/vietnam1995/hunglv/master/FacebookAutoLikeProfessional.user.js
 // @version  		13.0
 // ==/UserScript==
